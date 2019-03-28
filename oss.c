3147c8b087f63daa3007517eecc5c5d6256f4e65
Vasyl Onufriyev <projectclass41@gmail.com>
2019-03-27 22:57:17 -0500

    added shared memory and interupt handling

--- oss.c
+++ oss.c
@@ -1,6 +1,123 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/ipc.h> 
+#include <sys/shm.h> 
+#include <errno.h>
+#include "shared.h"
+#include <signal.h>
+#include <sys/time.h>
+#include "string.h"
+#include <sys/types.h>
+
+int ipcid; //inter proccess shared memory
+Shared* data; //shared memory data
+
+
+/* Create prototypes for used functions*/
+void handler(int signal);
+void DoFork(int value); 
+void ShmAttatch();
+void timerhandler(int sig);
+int setupinterrupt();
+int setuptimer();
+
+void handler(int signal) //handle ctrl-c and timer hit
+{
+	printf("%s: Kill Signal Caught. Killing children and terminating...", filen);
+	fflush(stdout);
+
+	shmctl(ipcid, IPC_RMID, NULL); //free shared mem
+
+	kill(getpid(), SIGTERM); //kill self
+}
+
+void DoFork(int value) //do fun fork stuff here. I know, very useful comment.
+{
+	char* forkarg[] = { //null terminated args set
+			"./user",
+			NULL
+	}; //null terminated parameter array of chars
+
+	execv(forkarg[0], forkarg); //exec
+	printf("Exec failed! Aborting."); //all is lost. we couldn't fork. Blast.
+	handler(1);
+}
+
+void ShmAttatch() //attach to shared memory
+{
+	key_t shmkey = ftok("shmshare", 765); //shared mem key
+
+	if (shmkey == -1) //check if the input file exists
+	{
+		printf("\n%s: ", filen);
+		fflush(stdout);
+		perror("Error: Ftok failed");
+		return;
+	}
+
+	ipcid = shmget(shmkey, sizeof(Shared), 0600 | IPC_CREAT); //get shared mem
+
+	if (ipcid == -1) //check if the input file exists
+	{
+		printf("\n%s: ", filen);
+		fflush(stdout);
+		perror("Error: failed to get shared memory");
+		return;
+	}
+
+	data = (Shared*)shmat(ipcid, (void*)0, 0); //attach to shared mem
+
+	if (data == (void*)-1) //check if the input file exists
+	{
+		printf("\n%s: ", filen);
+		fflush(stdout);
+		perror("Error: Failed to attach to shared memory");
+		return;
+	}
+}
+
+void timerhandler(int sig) //3 second kill timer
+{
+	handler(sig);
+}
+
+int setupinterrupt() //setup interrupt handling
+{
+	struct sigaction act;
+	act.sa_handler = timerhandler;
+	act.sa_flags = 0;
+	return (sigemptyset(&act.sa_mask) || sigaction(SIGPROF, &act, NULL));
+}
+
+int setuptimer() //setup timer handling
+{
+	struct itimerval value;
+	value.it_interval.tv_sec = 3;
+	value.it_interval.tv_usec = 0;
+	value.it_value = value.it_interval;
+	return (setitimer(ITIMER_PROF, &value, NULL));
+}
 
 int main()
 {
+	if (setupinterrupt() == -1) //handler for SIGPROF failed
+	{
+		perror("Failed to setup handler for SIGPROF");
+		return 1;
+	}
+	if (setuptimer() == -1) //timer failed
+	{
+		perror("Failed to setup ITIMER_PROF interval timer");
+		return 1;
+	}
+
+	ShmAttatch(); //attach to shared mem
+	signal(SIGINT, handler);
+
+	while(true);
+
 	return 0;
 }
---
+++ shared.h
@@ -0,0 +1,24 @@
+#ifndef SHARED_H
+
+#include <semaphore.h>
+
+typedef struct {
+    unsigned int seconds;
+    unsigned int ns;
+} Time;
+
+typedef struct {
+    int priority;
+    int pid;
+    Time tCpuTime;
+    Time tSysTime;
+    Time tBurTime;
+} ProcBlock;
+
+typedef struct {
+    ProcBlock proc[19];
+    Time sysTime;
+} Shared;
+
+#define SHARED_H
+#endif
