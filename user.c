fa2360d328ce427622117edf0067f645437c60a3
Vasyl Onufriyev <projectclass41@gmail.com>
2019-03-30 16:27:23 -0500

    asdfa

--- user.c
+++ user.c
@@ -143,9 +143,11 @@ int main(int argc, int argv)
 
 		AddTimeSpec(&unblockTime, secstoadd, mstoadd); //set unblock time to some value seconds value 0-5 and 0-1000ms but converted to ns to make my life easier
 
-		while ((data->sysTime.seconds >= unblockTime.seconds) && (data->sysTime.ns >= unblockTime.ns)) {
-					printf("Spinlock\n\n");
-		}
+		while (data->sysTime.seconds >= unblockTime.seconds)
+			while(data->sysTime.ns >= unblockTime.ns)
+				printf("Spinlocking...\n\n");
+
+		printf("POST SPINLOCK\n\n");
 		//wait on some task and block
 		exit(21);
 	}
